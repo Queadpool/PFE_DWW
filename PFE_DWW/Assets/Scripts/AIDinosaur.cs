4fefb7c51e6fbee4d026d100f54be8bb41bd1253
Queadpool <47141782+Queadpool@users.noreply.github.com>
2019-11-06 09:40:06 +0100

    ~ modif AI Script

---
+++ PFE_DWW/Assets/Scripts/AIDinosaur.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDinosaur : MonoBehaviour
+{
+    public enum State
+    {
+        Walk,
+        Eat,
+        Watch,
+        RunAway,
+        Soul,
+        Tame
+    }
+
+    [Header("Settings")]
+    [SerializeField] private bool carnivorous = true;
+
+    [Header("State Settings")]
+    [SerializeField] private GameObject stateSettings;
+
+    [Header("Walk Settings")]
+    [SerializeField] private GameObject walkSettings;
+
+    [Header("Eat Settings")]
+    [SerializeField] private GameObject eatSettings;
+
+    [Header("Watch Settings")]
+    [SerializeField] private GameObject watchSettings;
+
+    [Header("Run Away Settings")]
+    [SerializeField] private GameObject runSettings;
+
+    [Header("Soul Settings")]
+    [SerializeField] private GameObject soulSettings;
+
+    [Header("Tame Settings")]
+    [SerializeField] private GameObject tameSettings;
+
+    private void Start()
+    {
+        
+    }
+}
